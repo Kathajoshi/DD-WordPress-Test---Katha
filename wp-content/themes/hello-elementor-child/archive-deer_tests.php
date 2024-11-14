a82ebdba80438e84fe1f722db056fb74e7865634
Kathajoshi <kathajoshi23@gmail.com>
2024-11-14 11:09:56 +0530

    Final submission

--- wp-content/themes/hello-elementor-child/archive-deer_tests.php
+++ wp-content/themes/hello-elementor-child/archive-deer_tests.php
@@ -1,5 +1,10 @@
 <?php
-get_header(); ?>
+get_header(); 
+
+
+$start_date = get_field('start_date');
+$end_date = get_field('end_date');
+?>
 
 <div class="deer-tests-container">
     <h1 class="deer-tests-heading">Deer Tests</h1>
@@ -24,15 +29,47 @@ get_header(); ?>
             ?>
 
             <a class="deer-test-read-more" href="<?php the_permalink(); ?>">
-                <div class="card__header">
-                    <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" class="card__image"
-                        width="600">
+                <div class="main-header">
+
+                    <div class="card__header">
+                        <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" class="card__image"
+                            width="600">
+                    </div>
+                    
+                    <div class="card__body">
+                        <div class="date-ctm">
+    
+                            <?php if ($start_date) : ?>
+                            <span class="ctm-icon-box">
+                                <div class="image-ctm">
+                                    <img src="http://localhost/dd-wordpress-test/wp-content/uploads/2024/11/timetable.png"
+                                        alt="" srcset="">
+                                </div>
+                                <span class="ctm-p">
+                                    <span class="main-date">
+                                        <strong>Start Date:</strong>
+                                    </span>
+                                    <div class="main-date-ctm">
+                                        <?php echo esc_html($start_date); ?></div>
+                                </span>
+                            </span>
+                            <?php endif; ?>
+                            <?php if ($end_date) : ?>
+                            <span class="ctm-icon-box">
+                                <div class="image-ctm">
+                                    <img src="http://localhost/dd-wordpress-test/wp-content/uploads/2024/11/timetable.png"
+                                        alt="" srcset="">
+                                </div>
+                                <span class="ctm-p"><strong>End Date:</strong> <?php echo esc_html($end_date); ?></span>
+                            </span>
+                            <?php endif; ?>
+                        </div>
+                        <h4><?php the_title(); ?></h4>
+                        <div class="ctm-ptag"><?php the_excerpt(); ?></div>
+                    </div>
+
                 </div>
 
-                <div class="card__body">
-                    <h4><?php the_title(); ?></h4>
-                    <p><?php the_excerpt(); ?></p>
-                </div>
 
                 <div class="card__footer">
                     <div class="user">
